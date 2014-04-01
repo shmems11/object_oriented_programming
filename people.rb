9927a8f8f2b242cc3cc82c739939d4403cc5045d
shmems11 <shmems11@gmail.com>
2014-03-31 21:51:24 -0400

    first

--- people.rb
+++ people.rb
@@ -1,11 +1,23 @@
-class Student
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+  def greeting
+    puts "Hi, my name is #{name}."
+  end
+end
+
+class Student < Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object."
   end
 end
+
+
